c15e1233500fd131b07f0622da8224e01426b137
monicatangd <monicatangd@hotmail.com>
2022-10-19 12:46:00 -0500

    homework 06

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,7 +9,76 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+   this.left=null;
+   this.right=null;
+   this.value=value;
+}
+
+BinarySearchTree.prototype.size=function(){
+   let count =1;
+   if(this.left) count+=this.left.size();
+   if(this.right) count += this.right.size();
+   return count;
+}
+BinarySearchTree.prototype.insert=function(data){
+   if(data<this.value){
+      if(!this.left){
+         this.left= new BinarySearchTree(data);
+      }else{
+         this.left.insert(data);
+      }
+   }else{
+      if(!this.right){
+         this.right= new BinarySearchTree(data);
+      }else{
+         this.right.insert(data);
+      }
+   }
+}
+BinarySearchTree.prototype.contains=function(data){
+   if(data===this.value) return true;
+
+   if(this.left){
+      if(this.left.contains(data)) return true;
+   }
+   if(this.right){
+      if(this.right.contains(data)) return true;
+   }
+
+   return false;
+}
+BinarySearchTree.prototype.depthFirstForEach=function(cb, order= "in-order"){
+   switch(order){
+      case "in-order":{
+         this.left?.depthFirstForEach(cb, order);
+         cb(this.value);
+         this.right?.depthFirstForEach(cb, order);
+         break;
+      }
+      case "pre-order":{
+         cb(this.value);
+         this.left?.depthFirstForEach(cb, order);
+         this.right?.depthFirstForEach(cb, order);
+         break;
+      }
+      case "post-order":{
+         this.left?.depthFirstForEach(cb, order);
+         this.right?.depthFirstForEach(cb, order);
+         cb(this.value);
+         break;
+      }
+   }
+}
+BinarySearchTree.prototype.breadthFirstForEach=function(cb, queue=[]){
+   cb(this.value);
+   if(this.left) queue.push(this.left);
+   if(this.right) queue.push(this.right);
+
+   if(queue.length){
+      queue.shift().breadthFirstForEach(cb, queue);
+   }
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
